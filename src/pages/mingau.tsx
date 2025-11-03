54622153b667f5a49cf5912efee4b6e379941566
Gabriel Gardini <gabrielgardini1@gmail.com>
2025-11-03 09:45:16 -0300

    add redirect

---
+++ src/pages/mingau.tsx
@@ -0,0 +1,16 @@
+// pages/mingau.tsx
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+export default function MingauRedirect() {
+  const router = useRouter();
+
+//   useEffect(() => {
+//     // Redireciona após o carregamento da página
+//     window.location.href = "https://www.rocketseat.com.br/oferta/influencer/v2/gardinidev";
+//   }, []);
+
+  return (
+    <></>
+  );
+}
